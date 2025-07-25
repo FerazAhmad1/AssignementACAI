adf425e9250bab16c78cf43d18cb5ca54f43bf70
Feraz ahmad <ferazahmadgit62@gmail.com>
2025-07-25 22:57:22 +0530

    feat: mypage added

--- src/components/MyRequests.jsx
+++ src/components/MyRequests.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useItemContext } from "../context/useItemContext";
+import { ItemCard } from "./ItemCard";
 
 const MyRequests = () => {
-  return <div>MyRequests</div>;
+  const { items } = useItemContext();
+  const [myItems, setMyItems] = useState([]);
+  useEffect(() => {
+    let arr = [
+      { id: "itm001", status: "pending" },
+      { id: "itm003", status: "approve" },
+      { id: "itm002", status: "returned" },
+    ];
+    let result = [];
+    for (const item of items) {
+      for (const status of arr) {
+        if (status.id == item.id) {
+          result.push({ ...item, ...status });
+        }
+      }
+    }
+    setMyItems(() => result);
+  }, [myItems]);
+
+  return <ItemCard></ItemCard>;
 };
 
 export default MyRequests;
